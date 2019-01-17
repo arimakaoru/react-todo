1121babbfd0ea67236c8f6aa02516d01752e3885
Kaoru <kaoru.a992@gmail.com>
2019-01-17 22:28:18 +0900

    削除機能を追加

--- src/components/List.js
+++ src/components/List.js
@@ -3,46 +3,28 @@ import React from 'react';
 // import '../css/List.css'
 
 export default class List extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            checkedIndex: [],
-        };
-
-        this.handleCheckChange = this.handleCheckChange.bind(this);
-    }
-
-    handleCheckChange(changedItemIndex, event) {
-        if(event.target.checked){
-            this.setState({
-                checkedIndex: this.state.checkedIndex.concat(changedItemIndex),
-            });
-        }else{
-            this.setState({
-                checkedIndex: this.state.checkedIndex.filter((el) => {
-                    return el !== changedItemIndex;
-                }),
-            });
-        }
-    }
-
     render() {
-        const listItems = this.props.todoList.map((todo, index) => {
+        const listItems = this.props.todoList.map((todoObj, index) => {
             return (
                 <li key={index}>
                     <input
                         type="checkbox"
-                        checked={this.state.check}
-                        onChange={(event) => this.handleCheckChange(index, event)} />
-                    {todo}
+                        checked={todoObj.isChecked}
+                        onChange={(event) => this.props.toggleIsChecked(index, event)} />
+                    {todoObj.text}
                 </li>
             );
         });
 
         return (
-            <ul>
-                {listItems}
-            </ul>
+            <div className="list">
+                <ul>
+                    {listItems}
+                </ul>
+                <button
+                    type="button"
+                    onClick={this.props.deleteTodo}>削除する</button>
+            </div>
         );
     }
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -12,11 +12,32 @@ export default class Todo extends React.Component {
         };
 
         this.addTodo = this.addTodo.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.toggleIsChecked = this.toggleIsChecked.bind(this);
     }
 
     addTodo(todoText) {
         this.setState({
-            todoList: this.state.todoList.concat(todoText),
+            todoList: this.state.todoList.concat({
+                text: todoText,
+                isChecked: false,
+            }),
+        });
+    }
+
+    deleteTodo() {
+        this.setState({
+            todoList: this.state.todoList.filter((todoObj, index) => {
+                return !todoObj.isChecked;
+            }),
+        });
+    }
+
+    toggleIsChecked(index) {
+        const newTodoList = this.state.todoList;
+        newTodoList[index].isChecked = !this.state.todoList[index].isChecked;
+        this.setState({
+            todoList: newTodoList,
         });
     }
 
@@ -27,11 +48,11 @@ export default class Todo extends React.Component {
                 <Form
                     addTodo={this.addTodo}
                 />
-                <div className="list">
-                    <List
-                        todoList={this.state.todoList}
-                    />
-                </div>
+                <List
+                    todoList={this.state.todoList}
+                    deleteTodo={this.deleteTodo}
+                    toggleIsChecked={this.toggleIsChecked}
+                />
             </div>
         );
     }
