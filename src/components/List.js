42d453210e026fc6f0901345adf0d2390c9067f1
Kaoru <kaoru.a992@gmail.com>
2019-01-19 22:03:21 +0900

    リストの内、完了したアイテムに打ち消し線を追加

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-// import '../css/List.css'
+import '../css/List.css';
 
 export default class List extends React.Component{
     render() {
         const listItems = this.props.todoList.map((todoObj, index) => {
             return (
                 <li key={index}>
-                    <label>
+                    <label className={this.props.todoList[index].isDone ? 'list__text--done' : ''}>
                         <input
                             type="checkbox"
                             checked={todoObj.isChecked}
                             onChange={(event) => this.props.toggleIsChecked(index, event)} />
                         {todoObj.text}
                     </label>
-                    <button type="button" onClick={(event) => this.props.toggleIsDone(index, event)}>
+                    <button
+                        type="button"
+                        onClick={(event) => this.props.toggleIsDone(index, event)}>
                         {this.props.todoList[index].isDone ? '完了取り消し' : '完了する'}
                     </button>
                 </li>
